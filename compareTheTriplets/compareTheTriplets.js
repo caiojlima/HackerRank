da40838f285fbf9fd21db754914e92f33026f125
Caio Lima <caiojlimah@gmail.com>
2021-08-03 21:57:24 -0300

    compareTheTriplets resolution

---
+++ compareTheTriplets/compareTheTriplets.js
@@ -0,0 +1,13 @@
+function compareTriplets(a, b) {
+    let alice = 0;
+    let bob= 0;
+    for (let i = 0; i < a.length; i += 1;) {
+        if (a[i]> b[i]) {
+            alice += 1;
+        }else if (a[i] < b[i]) {
+            bob += 1;
+        } 
+
+    }
+    return [alice, bob];
+}

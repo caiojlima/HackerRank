6e1f4f33c7a53bcab1f0d98acb5d9952d30c3e1b
Caio Lima <caiojlimah@gmail.com>
2021-08-04 03:03:04 -0300

    staircase resolution

--- 7-staircase/staircase.js
+++ 7-staircase/staircase.js
@@ -1,4 +1,32 @@
-function staircase(n) {    
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function staircase(n) {
     for (let i = 1; i <= n; i += 1){
         let print = '';
         for (let i2 = 1; i2 <= n-i; i2 += 1) {
@@ -10,4 +38,9 @@ function staircase(n) {
         console.log(print); 
     }
 }
-console.log(staircase(5));
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    staircase(n);
+}
